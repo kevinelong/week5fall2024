43b5d29bc227ea89d46d6289020ed873ef0ff23a
kevinelong <kevinelong@gmail.com>
2024-10-21 10:54:23 -0700

    notes

--- data.js
+++ data.js
@@ -1,15 +1,21 @@
+// CTRL-C COPY // CTRL-V PASTE // CTRL-/ COMMENT/UNCOMMENT // CTRL-Z UNDO // CTRL-X CUT
+
 // Array(ordered) of Objects(key:value pairs)
-const data = [
-    { name: "Dandy", kind: "Dog" },
+const data = [ //creates an array [] AKA ordered list
+    { name: "Dandy", kind: "Dog" }, // {} create an object AKA key:value pairs
     { name: "Koshi", kind: "Dog" },
     { name: "Bianca", kind: "Cat" },
+    { name: "Bianca", kind: "Cat" },
+    { name: "Bianca", kind: "Cat" },
     { name: "Finley", kind: "Cat" },
-];
+]; //end array
 
 function petToString(pet) {
     return `
-        PET NAME: ${pet.name}
-        KIND: Good ${pet.kind}
+        MATH: ${ 6 * 7 } <-- ANY EXPRESSION \$\{\}
+        WORDS: ${ "AAA" + "BBB" } <-- string concatenation?
+        PET NAME:  ${ pet.name }
+        KIND: Good ${ pet.kind }
     `;
 }
 
@@ -25,7 +31,7 @@ function printPet(pet) {
 //     console.log(petToString(data[i])); // i-th
 // }
 
-//data.forEach(petToString); //rubberstamp each item with named function
+// data.forEach(petToString); //rubberstamp/repeat each item with named function
 
 // data.forEach((pet) => console.log(petToString(pet)))
 data.forEach(printPet);
